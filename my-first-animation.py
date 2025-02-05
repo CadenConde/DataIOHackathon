79493a310b841fc3327eb6426b9c0f5294c14782
Caden C. <cadenconde@gmail.com>
2025-02-05 00:48:18 -0500

    initial commit

---
+++ my-first-animation.py
@@ -0,0 +1,8 @@
+from manim import *
+
+class demo(Scene):
+    def construct(self):
+        t = Text("Hello!").shift(UP)
+        t2 = Tex("Hi!").shift(DOWN)
+        self.play(Write(t), Write(t2))
+        self.wait(3)
